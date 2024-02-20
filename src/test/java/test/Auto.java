9ece30ff6368ec4a8d3d1c31070f6faadc8ae465
PabloJimenez028 <juajimenezve@unal.edu.co>
2024-02-20 05:50:30 -0500

    Class Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,14 +1,34 @@
 package test;
 
-public class Auto {
-	String modelo;
-	int precio;
-	Asiento[] asientos;
-	String marca;
-	Motor motor;
-	int registro;
-	int cantidadCreados;
-	
-	
+public class Auto{
+    String modelo;
+    int precio;
+    Asiento [] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
 
+    public int cantidadAsientos() {
+        int cantidad = 0;
+        for (int i = 0; i < this.asientos.length; i++) {
+            if (this.asientos[i] != null) {
+                cantidadCreados++;
+            }
+        }
+
+    return cantidadCreados;
+    }
+
+    public String verificarIntegridad(){
+        for(int i = 0; i < this.asientos.length; i++){
+            if(this.asientos[i] != null) {
+                if (this.registro != this.asientos[i].registro || this.motor.registro != this.asientos[i].registro) {
+                    return "Las piezas no son originales";
+                }
+            }
+        }
+
+        return "Auto original";
+    }
 }
