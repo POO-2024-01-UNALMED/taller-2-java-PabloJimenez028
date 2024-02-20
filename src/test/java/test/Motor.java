2cdc6c75f8fbf73b39d9b397588431396d71dc34
PabloJimenez028 <juajimenezve@unal.edu.co>
2024-02-20 05:52:06 -0500

    Class Motor

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,9 +1,17 @@
 package test;
 
-public class Motor {
-	int numeroCilindros;
-	String tipo;
-	int registro;
-	
+public class Motor{
+    int numeroCilindros;
+    String tipo;
+    int registro;
 
+    void cambiarRegistro(int registro){
+        this.registro = registro;
+    }
+
+    void asignarTipo(String tipo){
+        if (tipo.equals("electrico") || tipo.equals("gasolina")) {
+            this.tipo = tipo;
+        }
+    }
 }
